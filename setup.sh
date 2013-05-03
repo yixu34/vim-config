f0f3ffaef09e8af8beb5b7cdb738723afad69b34
Yi Xu <yixu@ooyala.com>
2013-05-02 17:16:46 -0700

    Add get for coffeescript

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ mkdir -p ~/.vim/{ftdetect,indent,syntax}
 for d in ftdetect indent syntax ; do
     wget --no-check-certificate -O ~/.vim/$d/scala.vim https://raw.github.com/scala/scala-dist/master/tool-support/src/vim/$d/scala.vim;
 
+    wget --no-check-certificate -O ~/.vim/$d/coffee.vim https://raw.github.com/kchmck/vim-coffee-script/master/$d/coffee.vim;
 done
 
 for d in ftdetect syntax; do
