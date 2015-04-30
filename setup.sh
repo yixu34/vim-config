d642bdcb7d22dc50575b07daf05f12d48374c7ad
Yi Xu <yi.xu@adroll.com>
2015-04-29 18:42:05 -0700

    Add puppet

--- setup.sh
+++ setup.sh
@@ -8,6 +8,8 @@ for d in ftdetect indent syntax ; do
     wget --no-check-certificate -O ~/.vim/$d/coffee.vim https://raw.github.com/kchmck/vim-coffee-script/master/$d/coffee.vim;
 
     wget --no-check-certificate -O ~/.vim/$d/nginx.vim https://raw.github.com/yixu34/nginx-vim-syntax/master/$d/nginx.vim;
+
+    wget --no-check-certificate -O ~/.vim/$d/puppet.vim https://raw.github.com/puppetlabs/puppet-syntax-vim/master/$d/puppet.vim;
 done
 
 for d in ftdetect syntax; do
