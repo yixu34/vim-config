541656524d01bcd995fb80a1b1a5de56918b3808
Yi Xu <yi.xu@adroll.com>
2016-12-16 14:10:00 -0800

    Add terraform

--- setup.sh
+++ setup.sh
@@ -10,6 +10,8 @@ for d in ftdetect indent syntax ; do
     wget --no-check-certificate -O ~/.vim/$d/nginx.vim https://raw.githubusercontent.com/yixu34/nginx-vim-syntax/master/$d/nginx.vim;
 
     wget --no-check-certificate -O ~/.vim/$d/puppet.vim https://raw.githubusercontent.com/puppetlabs/puppet-syntax-vim/master/$d/puppet.vim;
+
+    wget --no-check-certificate -O ~/.vim/$d/terraform.vim https://raw.githubusercontent.com/hashivim/vim-terraform/master/$d/terraform.vim;
 done
 
 for d in ftdetect syntax; do
